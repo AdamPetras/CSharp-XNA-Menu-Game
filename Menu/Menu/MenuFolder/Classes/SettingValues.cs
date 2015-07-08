14dbacc5d231ebf999b8d1fa83c503815a18b6a2
abushrek <abushrek@seznam.cz>
2015-07-08 20:03:51 +0200

    https://github.com/abushrek/C-.git

--- Menu/Menu/MenuFolder/Classes/SettingValues.cs
+++ Menu/Menu/MenuFolder/Classes/SettingValues.cs
@@ -9,37 +9,10 @@ namespace Menu.MenuFolder.Classes
 
     public class SettingValues
     {
-        public enum EWidth
-        {
-            Vga = 640,
-            Xga = 1024,
-            Hd = 1280,
-            HdReady = 1600,
-            Fullhd = 1920
-        }
-
-        public enum EHeight
-        {
-            Vga = 480,
-            Xga = 768,
-            Hd = 720,
-            HdReady = 900,
-            Fullhd = 1080
-        }
-
         private Game game;
-        public EWidth eWidth { get; private set; }
-        public EHeight eHeight { get; private set; }
-        private int i;
-        public int width;
-        public int height;
         public SettingValues()
         {
-            eWidth = EWidth.HdReady;
-            eHeight = EHeight.HdReady;
-            width = (int)eWidth;
-            height = (int) EHeight.HdReady;
-            i = 3;
+
         }
 
         public SettingValues(Game game)
