c80004f121ae7627b25bba9a2d185caa390a6dfe
abushrek <abushrek@seznam.cz>
2015-08-30 22:44:47 +0200

    Implements Save/Load and added class for draw Obstactles

---
+++ Menu/Menu/GameFolder/Classes/Obstactle.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Menu.GameFolder.Classes
+{
+    public class Obstactle
+    {
+        public Vector2 Position { get; private set; }
+        public Texture2D Texture { get; private set; }
+        public bool Colision { get; private set; }
+
+        public Obstactle(Vector2 position,Texture2D texture, bool colision)
+        {
+            Position = position;
+            Texture = texture;
+            Colision = colision;
+        }
+    }
+}
