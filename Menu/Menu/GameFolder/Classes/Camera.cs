962f1ad926913d2d612a9f45e08c6e71d32e9cfa
abushrek <abushrek@seznam.cz>
2015-09-09 21:16:02 +0200

    Added character and go into and out of car

--- Menu/Menu/GameFolder/Classes/Camera.cs
+++ Menu/Menu/GameFolder/Classes/Camera.cs
@@ -8,13 +8,20 @@ namespace Menu.GameFolder.Classes
     {
         public Matrix Transform;
         public Vector2 Centering;
+        public Game game;
+
+        public Camera(Game game)
+        {
+            this.game = game;
+        }
+
         /// <summary>
         /// Method to update camera and center to some position
         /// </summary>
         /// <param name="objPosition"></param>
         public void Update(Vector2 objPosition)
         {
-            Centering = new Vector2(objPosition.X - Game.width / 2, objPosition.Y - Game.height / 2);
+            Centering = new Vector2(objPosition.X - game.graphics.PreferredBackBufferWidth/2, objPosition.Y - game.graphics.PreferredBackBufferHeight/2);
             Transform = Matrix.CreateScale(new Vector3(1, 1, 0)) *
                         Matrix.CreateTranslation(new Vector3(-Centering.X, -Centering.Y, 0));
         }
